113f56f60763b0f7167a91ccc989d49251c4a1c4
wangyudi <wangyudi@vip.qq.com>
2019-06-18 14:57:26 +0800

    模块提交

---
+++ design-pattern/design-pattern/design-factory/src/com/wyd/factory/demo1/Test.java
@@ -0,0 +1,15 @@
+package com.wyd.factory.demo1;
+
+/**
+ * @ClassName :Test
+ * @Description :
+ * @Author : wangyudi
+ * @Date : 2019/6/18 14:53
+ * @Version :1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
