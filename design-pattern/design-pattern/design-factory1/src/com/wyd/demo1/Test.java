c6397c02def1d36497c6723af156a8396499f95f
wangyudi <wangyudi@vip.qq.com>
2019-06-18 15:17:28 +0800

    模块提交

--- design-pattern/design-pattern/design-factory1/src/com/wyd/demo1/Test.java
+++ design-pattern/design-pattern/design-factory1/src/com/wyd/demo1/Test.java
@@ -1,15 +1,14 @@
-package com.wyd.factory.demo1;
+package com.wyd.demo1;
 
 /**
  * @ClassName :Test
  * @Description :
  * @Author : wangyudi
- * @Date : 2019/6/18 14:53
+ * @Date : 2019/6/18 15:13
  * @Version :1.0
  */
 public class Test {
-
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(13231);
     }
 }
