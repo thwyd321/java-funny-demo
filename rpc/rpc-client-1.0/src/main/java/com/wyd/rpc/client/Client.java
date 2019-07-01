e3591421e937c9ca2577935e5c8f9e021bde05de
wangyudi <wangyudi@vip.qq.com>
2019-07-01 17:39:28 +0800

    客户端新增动态代理模式

--- rpc/rpc-client-1.0/src/main/java/com/wyd/rpc/client/Client.java
+++ rpc/rpc-client-1.0/src/main/java/com/wyd/rpc/client/Client.java
@@ -20,14 +20,22 @@ import java.net.UnknownHostException;
 public class Client {
 
     public static void main(String[] args) {
-        IHelloService helloService = HelloServiceProxy.getInstance("2.0");
-        /*String result = helloService.hello("asdfasdfasddf");
-        System.out.println(result)*/;
         User user = new User();
         user.setAge(12);
         user.setName("fasdfa");
+
+        /**************静态代理****************/
+        IHelloService helloService = HelloServiceProxy.getInstance("2.0");
+        String result = helloService.hello("asdfasdfasddf");
+        System.out.println(result);
+
         String s = helloService.saveUser(user);
         System.out.println(s);
+        /**************动态代理****************/
+        DynamicProxy dynamicProxy = new DynamicProxy("2.0");
+        IHelloService helloService2 =  dynamicProxy.clientDynamicProxy(IHelloService.class,"localhost",8080);
+        String aa = helloService2.saveUser(user);
+        System.out.println(aa);
 
 
     }
---
+++ rpc/rpc-client-1.0/src/main/java/com/wyd/rpc/client/DynamicProxy.java
@@ -0,0 +1,80 @@
+package com.wyd.rpc.client;
+
+import com.wyd.rpc.server.api.RpcRequest;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * @ClassName :DynamicProxy
+ * @Description : 客户端动态代理
+ * @Author : wangyudi
+ * @Date : 2019/7/1 17:10
+ * @Version :1.0
+ */
+public class DynamicProxy {
+    private String version;
+
+    public DynamicProxy(String version) {
+        this.version = version;
+    }
+
+    public <T> T clientDynamicProxy(final Class interfaceClass, String host, int port){
+        return (T) Proxy.newProxyInstance(interfaceClass.getClassLoader(), new Class<?>[]{interfaceClass}, new InvocationHandler() {
+
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                RpcRequest rpcRequest = new RpcRequest();
+                rpcRequest.setVersion(version);
+                rpcRequest.setClassName(interfaceClass.getName());
+                rpcRequest.setMethodName(method.getName());
+                rpcRequest.setParameters(args);
+                return send(rpcRequest);
+            }
+        });
+    }
+    public Object send(RpcRequest rpcRequest) {
+        ObjectOutputStream objectOutputStream = null;
+        ObjectInputStream objectInputStream = null;
+        Object result = null;
+        try {
+            Socket socket = new Socket("localhost", 8080);
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.writeObject(rpcRequest);
+            objectOutputStream.flush();
+
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
+            result = objectInputStream.readObject();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (objectInputStream != null) {
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (objectOutputStream != null) {
+                try {
+                    objectOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        return result;
+    }
+
+}
